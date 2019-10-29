173c391cfcc50b40f4bb6cf977ba0092c813b059
GeneHFang <ghl234@nyu.edu>
2019-10-29 12:55:14 -0400

    Done.

--- app.rb
+++ app.rb
@@ -6,11 +6,20 @@ class App < Sinatra::Base
   end
 
   post '/reverse' do
+    string = params[:string]
+    @string = string.reverse 
     erb :reversed
   end
 
   get '/friends' do
     # Write your code here!
+    @array = [
+      "Emily Wilding Davison\n",
+      "Harriet Tubman\n",
+      "Joan of Arc\n",
+      "Malala Yousafzai\n",
+      "Sojourner Truth"
+    ]
 
   end
 end
